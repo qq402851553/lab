f882e482471fa01fc02e73760a87969b549bb1c2
labOfDsa <33450955+qq402851553@users.noreply.github.com>
2018-01-08 22:53:11 +0800

    Create Utility.h

---
+++ lab3/Utility.h
@@ -0,0 +1,23 @@
+#include<string.h>      //��׼������
+
+#include<iostream>    //��׼�����������
+
+#include<limits.h>      //�����޶�
+
+#include<math.h>        //��ѧ����
+
+#include<fstream>     //�ļ��������
+
+#include<ctype.h>       //�ַ�����
+
+#include<time.h>        //ʱ�亯��
+
+#include<conio.h>       //�������
+
+#include<stdlib.h>      //��׼��
+
+#include<stdio.h>       //��׼���������
+
+enum Error_code{success,fail,underflow,overflow,range_error};//������
+
+//enum bool{false,true};
